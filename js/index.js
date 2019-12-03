057ffa492e5471bd25aaf3dbd17c613c0909a654
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-03 12:42:53 -0800

    pricing info on mouseenter and mouseleave

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ destinationImage.addEventListener('dblclick', event => destinationImage.style.bo
 // keydown
 
 let newImagineSection = document.createElement('section');
-newImagineSection.innerHTML = "<h4>Just Imagine</h4><p>Where do you want to go? Click here and start typing.</p><p id=\"log\"></p>";
+newImagineSection.innerHTML = "<h4>Just Imagine</h4><p>Where do you want to go? Click here and start typing.</p><p id=\"log\"></p><div id=\"imagine-place\"><h2>vacation packages starting at $1979</h2></div>";
 newImagineSection.className = 'content-destination imagine';
 
 let homeContent = document.querySelector('.home');
@@ -40,6 +40,39 @@ document.addEventListener('keydown', event => {
     }
 });
 
+// mouseenter & mouseleave
+
+let imagineBox = document.getElementById('imagine-place');
+
+imagineBox.style.backgroundImage = 'url(\'img/person-on-lake.jpg\')';
+imagineBox.style.height = '200px';
+imagineBox.style.position = 'relative';
+
+let pricing = document.querySelector('#imagine-place h2');
+pricing.style.color = 'white';
+pricing.style.position = 'absolute';
+pricing.style.top = '50%';
+pricing.style.left = '50%';
+pricing.style.transform = 'translate(-50%, -50%)';
+pricing.style.lineHeight = '1.5';
+pricing.style.textAlign = 'center';
+
+pricing.style.display = 'none';
+
+imagineBox.addEventListener('mouseenter', (event) => {
+    imagineBox.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\'img/person-on-lake.jpg\')';
+    pricing.style.display = 'block';
+});
+
+imagineBox.addEventListener('mouseleave', () => {
+    imagineBox.style.backgroundImage = 'url(\'img/person-on-lake.jpg\')';
+    pricing.style.textShadow = '3px 3px black';
+});
+
+
+
+
+
 // mouseover
 //  wheel *
 //  drag / drop * 
@@ -48,13 +81,11 @@ document.addEventListener('keydown', event => {
 //  resize
 //  scroll
 //  select
-//  dblclick
-//  keydown or keypress
+
 // Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
 //  Nest two similar events somewhere in the site and prevent the event propagation properly
 //  Stop the navigation items from refreshing the page by using preventDefault()
 
-// alert
 // mouseenter
 // mouseleave
